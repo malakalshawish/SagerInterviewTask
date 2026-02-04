151c2bf451d584a932e9941efafffe0b3de2042b
malak <malakalshawish24@gmail.com>
2026-02-04 19:00:12 +0300

    Updated settings

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nbat=3yq&4)py5=e-g@c+xfm4ie&ek&ugqgqurtl+%jdvw07pd'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
