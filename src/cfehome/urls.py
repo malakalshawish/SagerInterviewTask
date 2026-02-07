e9c5a14ac1049efdd0f1a4ceb265efeca4b21ef9
malak <malakalshawish24@gmail.com>
2026-02-07 18:51:33 +0300

    removed core

--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -16,11 +16,10 @@ Including another URLconf
 """
 from . import views
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 
 urlpatterns = [
     path('', views.hello_world),
     path('health/', views.health_view),
     path('admin/', admin.site.urls),
-    path('core/', include('core.urls')),
 ]
